d722fa3f15e1898ef03cce32e4c4b828b6ba7d2d
LeahBasson <leahbasson54@gmail.com>
2025-04-02 20:36:16 +0200

    :tada:

---
+++ homework.py
@@ -0,0 +1,59 @@
+#Programming 1 - Homework
+#Leah Basson - 230146740
+
+#Outputs for loops
+# 1.a. output = 1 2 3 4 5
+# 1.b. output = 1 2 3
+# 1.c. output = 1 3 5
+# 1.d. output = 6 5 4 3 2    
+
+#2.Loop thats print my name 100 times.
+# Each output should be on a new line
+# for eachPass in range(100):
+#     print("Leah", sep=" ")
+    
+#3.Loop that prints the first 128 ASCII values followed by the corresponding characters
+#Number 3 ?
+
+#4.Loop that prints each character in a string
+#Number 4 ?
+
+#5.Using for loop and range() to print even numbers from 2 to 20.
+# for count in range(2,21,2):
+#     print(count, end =" ")
+
+#6.for loop that calculates the sum of numbers from 1 to 100
+# theSum = 0
+# for count in range(1, 101):
+#     theSum += count
+# print(theSum)
+
+#7. Given a list of numbers, use a for loop to find and print the maximum number.
+# max_num = 1
+# for count in range(1,5):
+#     max_num = max(max_num, count)
+# print(max_num)
+    
+#8. This is a list of test grades: 98, 76, 87, 71, 83, 90, 57, 79, 82, and 94.
+# Use the for loop to calculate and print of the class average.
+# grades = [98,76,87, 71, 83, 90, 57, 79, 82, 94]
+# theSum = 0
+# ave = 0
+# for count in grades:
+#     theSum += count
+#     length = len(grades)
+#     ave = theSum/length 
+# print(ave)
+
+#9. Use the range() and a for loop to calculate the total of the integers from 0 through 1,000,000
+# theSum = 0
+# for count in range(0, 1000001):
+#     theSum += count
+# print(theSum)
+
+#10.  Ask the user for a number and print its multiplication table up to 12.
+# number = int(input("Enter number:"))
+# for count in range(1, 13):
+#     print(f"{number} x {count} = {number * count}")
+
+    
